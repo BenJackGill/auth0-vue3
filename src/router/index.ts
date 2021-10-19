3fba6e478211b8a4f614db925ddecad1e7db6ccd
BenJackGill <2043257+BenJackGill@users.noreply.github.com>
2021-10-19 16:33:55 +1100

    Create API page

--- src/router/index.ts
+++ src/router/index.ts
@@ -22,6 +22,12 @@ const routes: Array<RouteRecordRaw> = [
     component: () =>
       import(/* webpackChunkName: "profile" */ "../views/Profile.vue"),
   },
+  {
+    path: "/faunaapi",
+    name: "FaunaApi",
+    component: () =>
+      import(/* webpackChunkName: "faunaapi" */ "../views/FaunaApi.vue"),
+  },
 ];
 
 const router = createRouter({
---
+++ src/views/FaunaApi.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>
+      Make external Fauna API call. Only logged in users can see this page and
+      perform this action.
+    </h1>
+  </div>
+</template>
